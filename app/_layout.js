5a29317d154813908f2fb4646f5fbc75139a254d
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-04-06 19:09:07 +0100

    Necessary app feature done

---
+++ app/_layout.js
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Stack onLayout={onLayoutRootView} />
+    </>
+  );
+};
+
+export default Layout;
